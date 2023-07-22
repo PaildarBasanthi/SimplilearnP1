9ed708c770022c213231304f3f4930d4fee8f263
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-22 15:24:52 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.IO;
+
+
+namespace ConminiProjectSimplilearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\basan\OneDrive\Desktop\Student Data.txt";
+
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine(fileContent);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found. Please make sure the file exists at the specified path.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
